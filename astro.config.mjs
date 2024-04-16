a10706b08fddbdc032468868b06a1b1966a155a7
Douglas Moore <douglas.bryant.moore@gmail.com>
2024-04-16 10:57:57 -0400

    Working on getting blog and tests setup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config"
-import react from "@astrojs/react"
+import vue from "@astrojs/vue"
 import tailwind from "@astrojs/tailwind"
+import mdx from "@astrojs/mdx"
+import viteEslint from "vite-plugin-eslint"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dbryantm.github.io",
   base: "/portfolio",
-  integrations: [react(), tailwind()],
+  integrations: [vue(), tailwind(), mdx()],
+  server: {
+    port: 8000,
+  },
+  vite: {
+    plugins: [viteEslint()],
+  },
 })
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,9 @@
 import astro from "eslint-plugin-astro"
-import react from "eslint-plugin-react"
-import reactHooks from "eslint-plugin-react-hooks"
+import vue from "eslint-plugin-vue"
 import prettier from "eslint-config-prettier"
 
 export default [
-  {
-    plugins: {
-      react: react,
-    },
-    rules: {
-      ...react.configs["jsx-runtime"].rules,
-    },
-  },
-  {
-    plugins: {
-      "react-hooks": reactHooks,
-    },
-    rules: reactHooks.configs.recommended.rules,
-  },
-  prettier,
   ...astro.configs["flat/recommended"],
+  ...vue.configs["flat/recommended"],
+  prettier,
 ]
