f2caff8d31a0736c9d85c889d2209e8f44b89447
Douglas Moore <douglas.bryant.moore@gmail.com>
2024-04-27 01:09:32 -0400

    Fixing the pathing for pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,12 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-import viteEslint from 'vite-plugin-eslint'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dbryantm.github.io',
-  base: '/portfolio',
   integrations: [react(), tailwind(), mdx()],
   server: {
     port: 8000,
-  },
-  vite: {
-    plugins: [
-      {
-        ...viteEslint(),
-        apply: 'serve',
-      },
-    ],
   },
 })
