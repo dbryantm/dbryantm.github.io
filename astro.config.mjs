fd96393c3edad0438c8d579df5a7e8d03a8aad32
Douglas Moore <douglas.bryant.moore@gmail.com>
2024-04-28 23:29:27 -0400

    Working on layout of the page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dbryantm.github.io',
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), icon()],
   server: {
     port: 8000,
   },
